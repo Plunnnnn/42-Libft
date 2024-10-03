d7a9220614b4d081a275c5948d3af824cff0ee72
0xBasmoussent <basmoussent@proton.me>
2024-10-03 22:53:10 +0200

    pipex structure

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: bdenfir <bdenfir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 21:55:58 by bdenfir           #+#    #+#             */
-/*   Updated: 2024/10/02 22:03:05 by bdenfir          ###   ########.fr       */
+/*   Updated: 2024/10/03 22:47:07 by bdenfir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,17 @@
 # include <string.h>
 # include "../libft/libft.h"
 
+typedef struct s_pipex {
+	int pipe_fd[2];
+
+} t_pipex;
+
+void parse_commande(char **argv);
+pid_t	fork_child(t_pipex *data, int pipe_fd[2], char **argv, char **envp);
+void create_pipe(t_pipex *data);
+void	error_exit(char *str);
+
+
 #endif
+
+
--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -1,106 +1,128 @@
 #include "pipex.h"
 
-void	error_exit(const char *msg)
+#include <sys/wait.h>
+
+
+char	*find_executable(char *cmd, char **envp)
 {
-	perror(msg);
+	char	**paths;
+	char	*path;
+	int		i;
+	char	*part_path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
+}
+
+t_pipex	*init_data(char *file1, char *file2)
+{
+	t_pipex	*data;
+	
+	data = malloc(sizeof(t_pipex));
+	if (!data)
+		error_exit("Allocatio faile for t-pipex structure");
+	
+	data->pipe_fd[0] = open(file1, O_RDWR);
+	data->pipe_fd[1] = open(file2, O_RDWR);
+	return (data);
+	// if (data->pipe_fd[0] < 1 || data->pipe_fd[1] < 1)
+	// {
+	// 	perror("error opening specified file");
+	// 	exit(EXIT_FAILURE);
+	// }
+	// if(dup2(data->pipe_fd[0], STDIN_FILENO) == -1)
+	// {
+	// 	perror("error redirection to STDIN_FILENO");
+	// 	exit(EXIT_FAILURE);
+	// }
+	// if(dup2(data->pipe_fd[1], STDOUT_FILENO) == -1)
+	// {
+	// 	perror("error redirection to STDOUT_FILENO");
+	// 	exit(EXIT_FAILURE);
+	// }
+}
+
+void	error_exit(char *str)
+{
+	
+	perror(str);
 	exit(EXIT_FAILURE);
 }
 
-void	exec_cmd(char *cmd, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	char **args = NULL;
-
-	args = ft_split(cmd, ' ');
-	if (!args)
-		error_exit("split error");
-
-	// Try to execute the command
-	if (execve(args[0], args, envp) == -1)
-		error_exit("execve error");
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	int		fd1, fd2, pipe_fd[2];
-	pid_t	pid1, pid2;
+	t_pipex *data;
+	pid_t pid1;
+	pid_t	pid2;
 
 	if (argc != 5)
-	{
-		fprintf(stderr, "Usage: %s file1 cmd1 cmd2 file2\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+		error_exit("Invalid argument count. Usage: ./pipex file1 cmd1 cmd2 file2");
+	data = init_data(argv[1], argv[argc-1]);
+	create_pipe(data);
+	pid1 = fork_child(data, data->pipe_fd, argv, envp);
+	pid2 = fork_child(data, data->pipe_fd, argv, envp);
+	printf("%d, %d", pid1, pid2);
+	//close_pipes_and_files(&data, pipe_fd);
+	//wait_for_children(pid1, pid2);
 
-	// Open file1 for reading
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 < 0)
-		error_exit("file1 open error");
-
-	// Open file2 for writing (create or truncate)
-	fd2 = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd2 < 0)
-		error_exit("file2 open error");
-
-	// Create the pipe
-	if (pipe(pipe_fd) == -1)
-		error_exit("pipe error");
-
-	// Fork the first child to execute cmd1
-	pid1 = fork();
-	if (pid1 == -1)
-		error_exit("fork error");
-
-	if (pid1 == 0) // Child 1 - execute cmd1
-	{
-		// Redirect stdin to file1
-		if (dup2(fd1, STDIN_FILENO) == -1)
-			error_exit("dup2 file1 error");
-		// Redirect stdout to pipe write-end
-		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-			error_exit("dup2 pipe write-end error");
-
-		// Close unused pipe ends and file descriptors
-		close(fd1);
-		close(fd2);
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-
-		// Execute cmd1
-		exec_cmd(argv[2], envp);
-	}
-
-	// Fork the second child to execute cmd2
-	pid2 = fork();
-	if (pid2 == -1)
-		error_exit("fork error");
-
-	if (pid2 == 0) // Child 2 - execute cmd2
-	{
-		// Redirect stdin to pipe read-end
-		if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
-			error_exit("dup2 pipe read-end error");
-		// Redirect stdout to file2
-		if (dup2(fd2, STDOUT_FILENO) == -1)
-			error_exit("dup2 file2 error");
-
-		// Close unused pipe ends and file descriptors
-		close(fd1);
-		close(fd2);
-		close(pipe_fd[0]);
-		close(pipe_fd[1]);
-
-		// Execute cmd2
-		exec_cmd(argv[3], envp);
-	}
-
-	// Parent process: Close all file descriptors and pipe ends
-	close(fd1);
-	close(fd2);
-	close(pipe_fd[0]);
-	close(pipe_fd[1]);
-
-	// Wait for both children to finish
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
-
-	return 0;
+	return (0);
+	
 }
+
+pid_t	fork_child(t_pipex *data, int pipe_fd[2], char **argv, char **envp)
+{
+	pid_t	pid;
+	char	*cmd;
+	char 	**args;
+
+	pid = fork();
+	args = ft_split(argv[1], ' ');
+	cmd = find_executable(args[0], envp);
+	printf("ICICICICI");
+	printf("%d, %s, %s", pid, cmd, args[1]);
+	if (pid < 1)
+		error_exit("error creating fork/child process");
+	if(pipe_fd[0] != STDIN_FILENO)
+	{
+		close(STDIN_FILENO);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[0]);
+	}
+	if (pipe_fd[1] != STDOUT_FILENO)
+	{
+		close(STDOUT_FILENO);
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[1]);
+	}
+	
+	if(execve(cmd, args, envp) == -1)
+		error_exit("failed to execute command");
+	printf("%d", data->pipe_fd[0]);
+	return(pid);
+	
+}
+
+void create_pipe(t_pipex *data)
+{
+	if (pipe(data->pipe_fd) == -1)
+		error_exit("failed toi create pipe");
+}
+
